6a1d53bd4fc9d995d9e00fe723dd8d6040e5e466
Chris <chipmanc@bellsouth.net>
2016-02-18 15:52:06 -0500

    Move celery initialization to app

--- django_stocks/settings.py
+++ django_stocks/settings.py
@@ -1,11 +1,3 @@
 from django.conf import settings
 
-DATA_DIR = settings.django_stocks_DATA_DIR = getattr(
-    settings,
-    'django_stocks_DATA_DIR',
-    '/tmp/django_stocks')
-
-settings.BROKER_URL = 'amqp://127.0.0.1'
-settings.CELERY_ACCEPT_CONTENT = ['json']
-settings.CELERY_TASK_SERIALIZER = 'json'
-settings.CELERY_RESULT_SERIALIZER = 'json'
+DATA_DIR = getattr(settings, 'django_stocks_DATA_DIR', '/tmp/django_stocks')
