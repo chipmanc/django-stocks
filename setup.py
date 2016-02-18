13013ab05c086d0196dbde9751ae508a0b93923d
Chris <chipmanc@bellsouth.net>
2016-02-18 16:03:43 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
         'Framework :: Django',
     ],
     zip_safe = False,
-<<<<<<< HEAD
-    install_requires = ['Django>=1.8.0', 'lxml', 'mock', 'celery'],
-=======
-    install_requires = ['django>=1.8.0', 'lxml', 'celery==3.1.18', 'MySQL-python==1.2.5'],
->>>>>>> develop
+    install_requires = ['Django>=1.8.0',
+                        'lxml',
+                        'mock',
+                        'celery==3.1.18',
+                        'MySQL-python==1.2.5'],
 )
