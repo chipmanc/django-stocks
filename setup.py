4a0a3d490445360bcddb6809bfdf58ff860d9772
Chris <chipmanc@bellsouth.net>
2016-02-29 17:17:48 -0500

    Add migrations back into git

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-stocks",
-    version = "0.5.5",
+    version = "0.5.6",
     packages = find_packages(),
     author = "Chris Chipman",
     author_email = "chipmanc@bellsouth.net",
